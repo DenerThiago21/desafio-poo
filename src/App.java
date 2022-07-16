39216393d234939426b96dd41facfff73d3a192e
Dener Thiago Ancini <denerthi.ibi@gmail.com>
2022-07-16 11:22:01 -0300

    classes, objetos e encapsulamento

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Java OO");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("NodeJS");
+        curso2.setDescricao("Node JS realTime");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Java Mentoria");
+        mentoria.setDescricao("mentoria de OO em java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
