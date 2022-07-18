a301939d1a88c2bae201fa2d74d4a8c8980b9611
Dener Thiago Ancini <denerthi.ibi@gmail.com>
2022-07-18 15:53:49 -0300

    finalizando o treinamento

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class App {
@@ -23,8 +25,40 @@ public class App {
         mentoria.setDescricao("mentoria de OO em java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java Dev");
+        bootcamp.setDescricao("Torne-se o maior javeiro de todos!");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devUser = new Dev();
+        devUser.setNome("user");
+        devUser.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos incritos user " + devUser.getConteudosInscritos());
+        
+        devUser.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos incritos user " + devUser.getConteudosInscritos());
+        System.out.println("Conteudos concluidos user " + devUser.getConteudosConcluidos());
+        System.out.println("XP: " + devUser.calcularTotalXp());
+
+        System.out.println("====================================");
+
+        Dev devSnow = new Dev();
+        devSnow.setNome("John Snow");
+        devSnow.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos incritos " + devSnow.getNome() + devSnow.getConteudosInscritos());
+        
+        devUser.progredir();
+        devUser.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos incritos " + devSnow.getNome() + devSnow.getConteudosInscritos());
+        System.out.println("Conteudos concluidos " + devSnow.getNome() + devSnow.getConteudosConcluidos());
+        System.out.println("XP: " + devUser.calcularTotalXp());
     }
 }
